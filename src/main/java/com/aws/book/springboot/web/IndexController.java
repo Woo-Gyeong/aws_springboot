9408dded8ac603c413682a509e6cb2144734a80e
lady823 <wg.jo@q-sol.co.kr>
2021-11-30 15:34:05 +0900

    로그인 공통 부분 어노테이션으로 변경

--- src/main/java/com/aws/book/springboot/web/IndexController.java
+++ src/main/java/com/aws/book/springboot/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.aws.book.springboot.web;
 
+import com.aws.book.springboot.config.auth.LoginUser;
 import com.aws.book.springboot.config.auth.dto.SessionUser;
 import com.aws.book.springboot.service.PostsService;
 import com.aws.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String Index(Model model) {
+    public String Index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        /*SessionUser user = (SessionUser) httpSession.getAttribute("user");*/
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
